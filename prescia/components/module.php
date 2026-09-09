5cd7767d73a0eaed4dc3282af2170a936bac79e1
leohmoraes <5.436213e+06+leohmoraes@users.noreply.github.com>
2026-09-09 00:09:18 +0000

    style: normalize notification indentation

--- prescia/components/module.php
+++ prescia/components/module.php
@@ -1943,10 +1943,10 @@ class CModule {
 	 		if ($action == CONS_ACTION_DELETE) {
 
 	 			$wS = ""; # whereStruct
-	 			$wTypes = "";
-	 			$wParams = array();
+				$wTypes = "";
+				$wParams = array();
 	 			$kA = array(); # keyArray
-	 			$module->getPreparedKeys($wS,$wTypes,$wParams,$kA,$data);
+				$module->getPreparedKeys($wS,$wTypes,$wParams,$kA,$data);
 	 			$data = $kA; // we want only the keys of the module which called the notification
 
 	 			if (isset($data[$module->keys[0]])) {
