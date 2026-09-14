f44bebc4a052e4dc1635126179e9a2b2610add47
leohmoraes <5.436213e+06+leohmoraes@users.noreply.github.com>
2026-09-14 00:16:54 +0000

    security: remove presciatester default credentials

--- pages/presciatester/_config/config.php
+++ pages/presciatester/_config/config.php
@@ -23,11 +23,11 @@
 		//$dev = $this->addPlugin('bi_dev');
 
 	} else { // <------------- settings for your local production machine
-		define("CONS_INSTALL_ROOT","/"); // if AFF is installed in other than the ROOT folder, fill this (must end and start with /)
-		define("CONS_DB_HOST","localhost"); // leave empty if not using a database. The framework will enter dbless mode
-		define("CONS_DB_BASE","presciatester");
-		define("CONS_DB_USER","root"); // Overridden by master, if set
-		define("CONS_DB_PASS","root"); // Overridden by master, if set
+			define("CONS_INSTALL_ROOT","/"); // if AFF is installed in other than the ROOT folder, fill this (must end and start with /)
+			define("CONS_DB_HOST","localhost"); // leave empty if not using a database. The framework will enter dbless mode
+			define("CONS_DB_BASE","presciatester");
+			define("CONS_DB_USER",(string)(getenv("PRESCIA_DB_USER") ?: ""));
+			define("CONS_DB_PASS",(string)(getenv("PRESCIA_DB_PASSWORD") ?: ""));
 
 		// you should have this enabled to help you while creating the site, then disable it for performance, or keep only on production (a.k.a. right here =p)
 		$dev = $this->addPlugin('bi_dev');
